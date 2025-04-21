afb3d3c810382462217174c8e07997183954cb05
sinzem <sinzem@gmail.com>
2025-04-21 22:55:12 +0300

    change_lang

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,23 @@ import languages from "./languages/languages";
 import styles from "./styles/page.module.css";
 
 import metadataGenerate from "../libs/metadataGenerate";
+import Link from "next/link";
 
 export const metadata: Metadata = metadataGenerate("en");
 
-export default function Home() {
+export default async function Home() {
 
   const {main} = languages("en");
 
   return (
     <div className={styles.page}>
       <h1>{main.title}</h1>
+      <Link href="/ua">
+        <button>Ua</button>
+      </Link>
+      <Link href="/ru">
+        <button>Ru</button>
+      </Link>
     </div>
   );
 }
---
+++ src/app/ua/page.tsx
@@ -0,0 +1,25 @@
+import { Metadata } from "next";
+import languages from "./../languages/languages";
+import styles from "../styles/page.module.css";
+
+import metadataGenerate from "../../libs/metadataGenerate";
+import Link from "next/link";
+
+export const metadata: Metadata = metadataGenerate("ua");
+
+export default async function HomeUa() {
+
+  const {main} = languages("ua");
+
+  return (
+    <div className={styles.page}>
+      <h1>{main.title}</h1>
+      <Link href="/">
+        <button>En</button>
+      </Link>
+      <Link href="/ru">
+        <button>Ru</button>
+      </Link>
+    </div>
+  );
+}
