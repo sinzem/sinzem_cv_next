f68620608f3a6574c5ba922fe5882341942cd230
sinzem <sinzem@gmail.com>
2025-05-08 22:29:52 +0300

    settings_mailer

--- src/app/api/mail/route.ts
+++ src/app/api/mail/route.ts
@@ -1,25 +1,42 @@
 import { NextResponse } from "next/server";
+import nodemailer from "nodemailer";
 
 export async function POST(request: Request) {
-    // const body = await request.json();
-    // console.log(body);
-    // const envData = process.env.NEXT_PUBLIC_GREETING;
-
-    // return NextResponse.json({message: "zaebalo", data: envData});
 
     try {
-        // Для JSON данных
-        const body = await request.json()
-        console.log("Тело запроса:", body)
+        const {name, email, message} = await request.json();
+
+        const transporter = nodemailer.createTransport({
+            host: process.env.NEXT_PUBLIC_EMAIL_HOST,
+            port: Number(process.env.NEXT_PUBLIC_EMAIL_PORT) || 587,
+            secure: process.env.NEXT_PUBLIC_EMAIL_SECURE === "true",
+            auth: {
+                user: process.env.NEXT_PUBLIC_EMAIL_USER,
+                pass: process.env.NEXT_PUBLIC_EMAIL_PASSWORD,
+            },
+        })
+
+        const mailOptions = {
+            from: process.env.NEXT_PUBLIC_EMAIL_FROM,
+            to: process.env.NEXT_PUBLIC_EMAIL_TO,
+            subject: `New message from ${name}`,
+            text: message,
+            html: `
+                <div>
+                    <h1>New message from CV on Vercel</h1>
+                    <p><strong>Name:</strong> ${name}</p>
+                    <p><strong>Email:</strong> ${email}</p>
+                    <p><strong>Message:</strong> ${message}</p>
+                </div>
+            `,
+        }
+
+        await transporter.sendMail(mailOptions);
     
-        // Обработка данных
-        const { name , email} = body
-    
-        // Возвращаем ответ
-        return NextResponse.json({ message: "Пользователь создан", user: { name , email} }, { status: 201 })
-      } catch (error) {
-        console.error("Ошибка при обработке запроса:", error)
-        return NextResponse.json({ error: "Ошибка при обработке запроса" }, { status: 400 })
-      }
+        return NextResponse.json({ message: "Sending successful" }, { status: 200 })
+    } catch (error) {
+        console.error("Error sending email:", error)
+        return NextResponse.json({ error: "Error sending email" }, { status: 500 })
+    }
 } 
 
