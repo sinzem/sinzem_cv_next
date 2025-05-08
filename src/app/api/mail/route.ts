9ab541eb99d972e0edf4f9c9fa283397587aac03
sinzem <sinzem@gmail.com>
2025-05-08 22:36:02 +0300

    fix_env_pathes

--- src/app/api/mail/route.ts
+++ src/app/api/mail/route.ts
@@ -7,18 +7,18 @@ export async function POST(request: Request) {
         const {name, email, message} = await request.json();
 
         const transporter = nodemailer.createTransport({
-            host: process.env.NEXT_PUBLIC_EMAIL_HOST,
-            port: Number(process.env.NEXT_PUBLIC_EMAIL_PORT) || 587,
-            secure: process.env.NEXT_PUBLIC_EMAIL_SECURE === "true",
+            host: process.env.EMAIL_HOST,
+            port: Number(process.env.EMAIL_PORT) || 587,
+            secure: process.env.EMAIL_SECURE === "true",
             auth: {
-                user: process.env.NEXT_PUBLIC_EMAIL_USER,
-                pass: process.env.NEXT_PUBLIC_EMAIL_PASSWORD,
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASSWORD,
             },
         })
 
         const mailOptions = {
-            from: process.env.NEXT_PUBLIC_EMAIL_FROM,
-            to: process.env.NEXT_PUBLIC_EMAIL_TO,
+            from: process.env.EMAIL_FROM,
+            to: process.env.EMAIL_TO,
             subject: `New message from ${name}`,
             text: message,
             html: `
@@ -39,4 +39,47 @@ export async function POST(request: Request) {
         return NextResponse.json({ error: "Error sending email" }, { status: 500 })
     }
 } 
+
+
+// import { NextResponse } from "next/server";
+// import nodemailer from "nodemailer";
+
+// export async function POST(request: Request) {
+
+//     try {
+//         const {name, email, message} = await request.json();
+
+//         const transporter = nodemailer.createTransport({
+//             host: process.env.NEXT_PUBLIC_EMAIL_HOST,
+//             port: Number(process.env.NEXT_PUBLIC_EMAIL_PORT) || 587,
+//             secure: process.env.NEXT_PUBLIC_EMAIL_SECURE === "true",
+//             auth: {
+//                 user: process.env.NEXT_PUBLIC_EMAIL_USER,
+//                 pass: process.env.NEXT_PUBLIC_EMAIL_PASSWORD,
+//             },
+//         })
+
+//         const mailOptions = {
+//             from: process.env.NEXT_PUBLIC_EMAIL_FROM,
+//             to: process.env.NEXT_PUBLIC_EMAIL_TO,
+//             subject: `New message from ${name}`,
+//             text: message,
+//             html: `
+//                 <div>
+//                     <h1>New message from CV on Vercel</h1>
+//                     <p><strong>Name:</strong> ${name}</p>
+//                     <p><strong>Email:</strong> ${email}</p>
+//                     <p><strong>Message:</strong> ${message}</p>
+//                 </div>
+//             `,
+//         }
+
+//         await transporter.sendMail(mailOptions);
+    
+//         return NextResponse.json({ message: "Sending successful" }, { status: 200 })
+//     } catch (error) {
+//         console.error("Error sending email:", error)
+//         return NextResponse.json({ error: "Error sending email" }, { status: 500 })
+//     }
+// } 
 
