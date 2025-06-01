60518c1aa664da9fea1a9c300296a575a1951c7d
sinzem <sinzem@gmail.com>
2025-06-01 20:44:10 +0300

    fix_cases

--- src/assets/cases/basket.ts
+++ src/assets/cases/basket.ts
@@ -76,16 +76,16 @@ const basketCases: ICase[] = [
         url: "/assets/img/cases_block/basket-cases_img/delivery.png",
         link: "https://sinzem.github.io/cases/basket/delivery/index.html#our_advantages",
     },
-    {
-        id: "b0016",
-        url: "/assets/img/cases_block/basket-cases_img/building.jpg",
-        link: "https://sinzem.github.io/cases/basket/building/index.html",
-    },
-    {
-        id: "b0017",
-        url: "/assets/img/cases_block/basket-cases_img/banner.jpg",
-        link: "https://sinzem.github.io/cases/basket/dental_clinic/index.html",
-    },
+    // {
+    //     id: "b0016",
+    //     url: "/assets/img/cases_block/basket-cases_img/building.jpg",
+    //     link: "https://sinzem.github.io/cases/basket/building/index.html",
+    // },
+    // {
+    //     id: "b0017",
+    //     url: "/assets/img/cases_block/basket-cases_img/banner.jpg",
+    //     link: "https://sinzem.github.io/cases/basket/dental_clinic/index.html",
+    // },
     {
         id: "b0018",
         url: "/assets/img/cases_block/basket-cases_img/advantage_top_left.jpg",
